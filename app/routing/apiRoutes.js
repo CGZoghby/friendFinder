d9d4fa8657fae453780dbb92ab03d931a44879ad
Christopher Zoghby <cgzoghby@gmail.com>
2018-04-28 17:36:55 -0400

    just need to do score math

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,6 +1,4 @@
 var friendsData = require("../data/friends");
-
-
 // ===============================================================================
 // ROUTING
 // ===============================================================================
@@ -12,8 +10,11 @@ module.exports = function (app) {
         res.json(friendsData);
     });
     //a POST route with "/api/friends" which will handle incoming survey results. Also be used to handle compatability logic.
-    app.post("/api/friends", function (res, req) {
-        //call on something like friendsData[i].scores
+    app.post("/api/friends", function (req, res) {
+        //now need to compare the user test scores against all others. But first lets make sure these scores exist
+        friendsData.push(req.body);
+        //console.log(JSON.stringify(req.body.scores)); this is the path for the just-submitted scores, to compare against database ones.
+        console.log(friendsData)
     });
     //compatability logic: convert (or store) each user's results as a simple array of numbers  (ex: `[5, 1, 4, 4, 5, 1, 2, 5, 4, 1]`)
     // compare the scores from two users by taking the difference (absolute value) between their scores on each question. 
